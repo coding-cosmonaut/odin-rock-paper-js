106136c298d03fc57151029870a84e536ca116f8
Alex <aamorgun2003@gmail.com>
2023-12-14 17:55:25 -0700

    Create new branch/add UI
    Added new buttons that allows the user to play the game without
    typing into a prompt. No longer need game() function. Refactored
    playRound() in order to accept events and call a new function when
    the player or computer reaches a score of 5.
    
    The new endGame() updates global variables after the game is over.

---
+++ index.js
@@ -0,0 +1,77 @@
+let computerScore = 0;
+let playerScore = 0;
+const resultsDiv = document.querySelector("#results");
+const currentScore = document.querySelector('p');
+const playBtns = document.querySelectorAll("button");
+const playBtnsArr = Array.from(playBtns).map((item) => {
+  item.addEventListener("click", playRound);
+  return item;
+});
+currentScore.textContent = `Current Score: Player:${playerScore} Computer:${computerScore}`
+
+
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * 3);
+  if (randomNumber === 0) {
+    return "rock";
+  } else if (randomNumber === 1) {
+    return "paper";
+  } else if (randomNumber === 2) {
+    return "scissors";
+  } else {
+    return "Error";
+  }
+}
+
+function playRound(
+  e,
+  playerSelection = e.target.textContent,
+  computerSelection = getComputerChoice()
+) {
+  let player = playerSelection.toLowerCase();
+  if (player === computerSelection) {
+    return (resultsDiv.innerHTML = `Tie! Please select again!`);
+  } else if (player !== computerSelection) {
+    if (player === "rock") {
+      if (computerSelection === "scissors") {
+        playerScore += 1;
+        currentScore.textContent = `Current Score: Player:${playerScore} Computer:${computerScore}`
+        return playerScore === 5
+          ? endgame()
+          : (resultsDiv.innerHTML = `You win! ${player} beats ${computerSelection}.`);
+      } else {
+        computerScore += 1;
+        currentScore.textContent = `Current Score: Player:${playerScore} Computer:${computerScore}`
+        return computerScore === 5
+          ? endgame()
+          : (resultsDiv.innerHTML = `You lose! ${computerSelection} beats ${player}.`);
+      }
+    } else if (player === "paper") {
+      if (computerSelection === "rock") {
+        return `You win! Paper beats Rock`;
+      } else {
+        return `You lose! Scissors beats paper!`;
+      }
+    } else if (player === "scissors") {
+      if (computerSelection === "paper") {
+        return `You win! Scissors beats Paper!`;
+      } else {
+        return `You lose! Rock beats Scissors!`;
+      }
+    }
+  } else {
+    console.log("IDK");
+  }
+}
+
+function endgame() {
+  if (playerScore === 5) {
+    computerScore = 0;
+    playerScore = 0;
+    return (resultsDiv.innerHTML = "You beat the computer! -- <strong>Select again to restart.</strong>");
+  } else {
+    computerScore = 0;
+    playerScore = 0;
+    return resultsDiv.innerHTML = "Computer beat ya. -- <strong>Select again to restart.</strong>";
+  }
+}
